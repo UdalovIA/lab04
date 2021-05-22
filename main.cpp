19161934fb013e7e46c68895c044e4bc3796b78a
Alice (IvanovII) <alice@example.com>
2021-05-23 02:14:26 +0300

    error

--- main.cpp
+++ main.cpp
@@ -11,6 +11,8 @@ int main(int argc, char* argv[]) {
                 CURLcode res;
                 curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
                 res = curl_easy_perform(curl);
+                if (CURLE_OK != 0)
+                     curl_easy_strerror(res);
                 curl_easy_cleanup(curl);
 }
         return 0;
