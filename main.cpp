da67cd8cea1957565d97a3b9f61acabaae972317
Alice (IvanovII) <alice@example.com>
2021-05-23 01:48:19 +0300

    argc

--- main.cpp
+++ main.cpp
@@ -4,7 +4,12 @@
 #include <curl/curl.h>
 using namespace std;
 
-int main() {
+int main(int argc, char* argv[]) {
+    if (argc > 1)
+        for (int i = 0; i < argc; i++){
+            cerr << argv[i];
+            return 0;
+        }
     curl_global_init(CURL_GLOBAL_ALL);
     // Ввод данных
     const auto input = read_input(cin, true);
